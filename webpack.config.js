02dbed6c880a303f42c0f0f22c56b7e29a449833
jodobear <jodobear@protonmail.com>
2019-11-09 02:01:05 +0100

    fixed comma in webpack-config

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,10 @@ module.exports = {
         compress: true  // optimazation
     },
     watch: true,  // so that we don't to restart webpack server all the time
-    devtool: 'source-map'  // Has few options - source-map = maps code, transpired coce, minified code, etc. Understand how this works.
+    devtool: 'source-map',  // Has few options - source-map = maps code, transpired coce, minified code, etc. Understand how this works.
     output: {
         filename: `[name].bundle.js`,  // output name of the webpack bundle
         path: path.resolve(__dirname, 'dist')  // .resolve: get current dir and concatenates it with the files and such.
         // dist: output folder (also concatenated to current dir through path). Distribution directory containing bundles for production. Source will be in src
-    }
+    },
 }
