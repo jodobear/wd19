292cb1010a3a8443eff9388af3c0b657f391390f
jodobear <jodobear@protonmail.com>
2019-11-14 18:12:08 +0100

    learnt to build the app for distribution, the dist folder and touched upon ASP.NET, Django framework templates.

--- src/components/image.js
+++ src/components/image.js
@@ -4,7 +4,7 @@ export default class Image extends React.Component {
     render() {
         let customClass = this.props.classes ? this.props.classes : "img-fluid"
         return (
-            <img src="http://rxi.iscdn.net/2016/03/120948_windowlicker.jpg" class={ customClass } alt="Responsive image"></img>
+            <img src="http://rxi.iscdn.net/2016/03/120948_windowlicker.jpg" className={ customClass } alt="Responsive image"></img>
         )
     }
 }
