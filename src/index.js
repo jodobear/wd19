631e5861d2279753a007a1056a071e8525a76b41
jodobear <jodobear@protonmail.com>
2019-11-14 15:44:30 +0100

    configured custom styling and looked at devtools in Chrome - source maps & React. Then configured Flexbox and demoed how to use it.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './sass/bootstrap/scss/bootstrap.scss';
+import './sass/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from "./pages/home";
 
 ReactDOM.render(
-    <Home></Home>,
+    <Home title="Given title where instantiated, in `index.js`"></Home>,
     document.getElementById('root')
 )
--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,7 @@ export default class Home extends React.Component {
         }
         return (
             <div>
-                <Header title="Windowlicker"></Header>
+                <Header title={this.props.title ? this.props.title : "no title given nor where this component is instantiated."}></Header>
                 <Grid data={[<div><h1>holy crap, an html header</h1></div>, "Second column Title", "THIRD", person, "Funk" + func()]} />
             </div>
         )
