28045264899c7ff8eed99f104da61a1064c2c384
jodobear <jodobear@protonmail.com>
2019-11-15 20:51:25 +0100

    updated the course component to take variables instead of hardcoded data

--- src/components/course.js
+++ src/components/course.js
@@ -2,22 +2,21 @@ import React from 'react';
 
 export default class Course extends React.Component {
     render() {
+        let data = {
+            title: "Web Development in 2019 - From Start to Finish",
+            discription: "In this course we will be looking at building a production level React UI project using the latest web development technologies. Upon completing this course you should have a solid understanding of the technologies used by a typical web developer in 2019. The course covers Webpack, Babel, SASS, CSS, HTML, JavaScript (ES6), React, Bootstrap and many other technologies and techniques.",
+            level: "Beginner",
+            link: "https://www.udemy.com/web-development-in-2019-from-start-to-finish/?couponCode=TENPERCENT"
+        }
         return (
             <div className="resume-item d-flex flex-column flex-md-row mb-5">
                 <div className="resume-content mr-auto">
-                <h3 className="mb-0">Web Development in 2019 - From Start to Finish</h3>
-                <div className="subheading mb-3">Beginner</div>
-                    <p>In this course we will be looking at building a production
-                        level React UI project using the latest web development
-                        technologies. Upon completing this course you should have
-                        a solid understanding of the technologies used by a typical
-                        web developer in 2019.
-                        The course covers Webpack, Babel, SASS, CSS, HTML, JavaScript
-                        (ES6), React, Bootstrap and many other technologies and techniques.
-                    </p>
+                <h3 className="mb-0">{data.title}</h3>
+                <div className="subheading mb-3">{data.level}</div>
+                    <p>{data.discription}</p>
                 </div>
             <div className="resume-date text-md-right">
-                <a href="https://www.udemy.com/web-development-in-2019-from-start-to-finish/?couponCode=TENPERCENT"><span className="text-primary">View More</span></a>
+                <a href={data.link}><span className="text-primary">View More</span></a>
             </div>
         </div>
         )
